76859e8f66a34d5e8528d8c85ce2c7b6f624a319
Saulo Muniz <saulomuniz.dev@gmail.com>
2025-09-15 22:12:49 -0300

    visao form ajuda

--- src/Pages/FormQueroAjuda.tsx
+++ src/Pages/FormQueroAjuda.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import style from './../Style/saulo.module.css';
 
 function FormQueroAjuda () {
 
@@ -35,46 +36,38 @@ function FormQueroAjuda () {
         setEstado(evento.target.value);
     }
 
-
-
-
-    
-
-  
-
-    
-
-    
-
-
-
     return(
 
-        <div className="divPaiForAjuda">
+        <div className={style.divPaiForAjuda}>
             <h1>FORMULÁRIO DE AJUDA</h1>
-            <h2 className="formH2">Informe todas as informações abaixo </h2>
-            <div className="FormAjuda">
-                <p className="FormParagrafo">Nome</p>
-                <input className="FormInput" onChange={modificarNome} type="text" placeholder="Digite seu nome" /> 
-                {/* <br />Instituição: {nome} <br /><br /> */}
-                <hr className="ForHr"/>
-                <p className="FormParagrafo">Telefone</p>
-                <input className="FormInput" onChange={modificarTelefone} type="text" placeholder="Digite seu telefone" /> 
-                {/* <br />Ajuda: {ajuda} <br /><br /> */}
-                <hr className="ForHr" />
-                <p className="FormParagrafo">Email</p>
-                <input className="FormInput" onChange={modificarEmail} type="text" placeholder="Digite seu e-mail" /> 
-                {/* <br /> Nome: {nome} <br /><br /> */}
-                <hr className="ForHr" />
-                <p className="FormParagrafo">Assunto</p>
-                <input className="FormInput"  onChange={modificarAssunto} type="text" placeholder="Digite a doença" /> 
-                {/* <br /> Telefone: {telefone} <br /><br /> */}
-                <hr className="ForHr"/>
-                <p className="FormParagrafo">Email</p>
-                <input className="FormInput"  onChange={modificarEmail} type="text" placeholder="Digite seu email" /> 
-                {/* <br /> Email: {email} <br /><br />  */}
-                <button className="btoFormAjuda" >Enviar</button>
-                <button className="btoFormAjuda">Limpar</button>
+            <h2 className={style.formH2}>Informe todas as informações abaixo </h2>
+            <div className={style.FormAjuda}>
+                <p className={style.FormParagrafo}>Nome</p>
+                <input className={style.FormInput} onChange={modificarNome} type="text" placeholder="Digite seu nome" /> 
+                <p className={style.FormParagrafo}>Telefone</p>
+                <input className={style.FormInput} onChange={modificarTelefone} type="text" placeholder="Digite seu telefone" /> 
+                <p className={style.FormParagrafo}>Email</p>
+                <input className={style.FormInput} onChange={modificarEmail} type="text" placeholder="Digite seu e-mail" /> 
+                <p className={style.FormParagrafo}>Assunto</p>
+                <input className={style.FormInput}  onChange={modificarAssunto} type="text" placeholder="Digite a doença" /> 
+                <p className={style.FormParagrafo}>Cidade</p>
+                <input className={style.FormInput}  onChange={modificarCidade} type="text" placeholder="Digite o nome da sua cidade" /> 
+                <p className={style.FormParagrafo}>Estado</p>
+                <input className={style.FormInput}  onChange={modificarEstado} type="text" placeholder="Digite o nome do seu estado" /> 
+                <p className={style.FormParagrafo}>O que você sente?</p>
+
+                <input type="checkbox" /> Tremor <br />
+                <input type="checkbox" /> Cansaço <br />
+                <input type="checkbox" /> Desanimo <br />
+                <input type="checkbox" /> Falta de ar <br />
+                <input type="checkbox" /> Agonia <br />
+                <input type="checkbox" /> Falta de foco <br />
+                <input type="checkbox" /> Alteração de humor <br />
+                <input type="checkbox" /> Sensação de desconexão <br />
+                <input type="checkbox" /> Preocupação com peso <br /><br />
+
+                <button className={style.btoFormAjuda} >Enviar</button>
+                <button className={style.btoFormAjuda}>Limpar</button>
                 <br />
             </div>
         </div>
