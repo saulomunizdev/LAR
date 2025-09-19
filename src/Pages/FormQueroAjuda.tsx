7b678427216fb4899edd97009d53d1a0ddc021eb
Saulo Muniz <saulomuniz.dev@gmail.com>
2025-09-18 21:52:19 -0300

    FormAjuda

--- src/Pages/FormQueroAjuda.tsx
+++ src/Pages/FormQueroAjuda.tsx
@@ -37,41 +37,68 @@ function FormQueroAjuda () {
     }
 
     return(
-
-        <div className={style.divPaiForAjuda}>
-            <h1>FORMULÁRIO DE AJUDA</h1>
-            <h2 className={style.formH2}>Informe todas as informações abaixo </h2>
-            <div className={style.FormAjuda}>
-                <p className={style.FormParagrafo}>Nome</p>
-                <input className={style.FormInput} onChange={modificarNome} type="text" placeholder="Digite seu nome" /> 
-                <p className={style.FormParagrafo}>Telefone</p>
-                <input className={style.FormInput} onChange={modificarTelefone} type="text" placeholder="Digite seu telefone" /> 
-                <p className={style.FormParagrafo}>Email</p>
-                <input className={style.FormInput} onChange={modificarEmail} type="text" placeholder="Digite seu e-mail" /> 
-                <p className={style.FormParagrafo}>Assunto</p>
-                <input className={style.FormInput}  onChange={modificarAssunto} type="text" placeholder="Digite a doença" /> 
-                <p className={style.FormParagrafo}>Cidade</p>
-                <input className={style.FormInput}  onChange={modificarCidade} type="text" placeholder="Digite o nome da sua cidade" /> 
-                <p className={style.FormParagrafo}>Estado</p>
-                <input className={style.FormInput}  onChange={modificarEstado} type="text" placeholder="Digite o nome do seu estado" /> 
-                <p className={style.FormParagrafo}>O que você sente?</p>
-
-                <input type="checkbox" /> Tremor <br />
-                <input type="checkbox" /> Cansaço <br />
-                <input type="checkbox" /> Desanimo <br />
-                <input type="checkbox" /> Falta de ar <br />
-                <input type="checkbox" /> Agonia <br />
-                <input type="checkbox" /> Falta de foco <br />
-                <input type="checkbox" /> Alteração de humor <br />
-                <input type="checkbox" /> Sensação de desconexão <br />
-                <input type="checkbox" /> Preocupação com peso <br /><br />
-
-                <button className={style.btoFormAjuda} >Enviar</button>
-                <button className={style.btoFormAjuda}>Limpar</button>
-                <br />
+        <div className={style.divFormGeral}>
+            
+            <div className={style.divLateral}>
+                <h1>Olá!</h1>
+                <hr />
+                <p> Estamos felizes por você estar dando esse grande passso. </p>
+                <p> Calma, tudo vai melhorar só precisamos que você responda o formulário para entedermos melhor como podemos te ajudar. </p>
+                <p> Em casos de emergência para assunstos psicologiocos ligue para <strong>188</strong> ou <strong>190</strong>.</p>
+                <p> Em casos de emergência para abusos ligue para <strong>188</strong> ou <strong>190</strong>.</p>
             </div>
+
+            <div className={style.divHs}>
+                <h1 className={style.divH1}>FORMULÁRIO DE AJUDA</h1>
+                <h2 className={style.divH2}>Informe todas as informações abaixo </h2>
+            </div>
+
         </div>
     )
 }
 
 export default FormQueroAjuda;
+
+
+            // <div className={style.divPaiForAjuda}>
+                
+                    
+            //     <div className={style.FormAjuda}>
+            //         <p className={style.FormParagrafo}>Nome</p>
+            //         <input className={style.FormInput} onChange={modificarNome} type="text" placeholder="Digite seu nome" /> 
+            //         <p className={style.FormParagrafo}>Telefone</p>
+            //         <input className={style.FormInput} onChange={modificarTelefone} type="text" placeholder="Digite seu telefone" /> 
+            //         <p className={style.FormParagrafo}>Email</p>
+            //         <input className={style.FormInput} onChange={modificarEmail} type="text" placeholder="Digite seu e-mail" /> 
+            //         <p className={style.FormParagrafo}>Assunto</p>
+            //         <input className={style.FormInput}  onChange={modificarAssunto} type="text" placeholder="Digite a doença" /> 
+            //         <p className={style.FormParagrafo}>Cidade</p>
+            //         <input className={style.FormInput}  onChange={modificarCidade} type="text" placeholder="Digite o nome da sua cidade" /> 
+            //         <p className={style.FormParagrafo}>Estado</p>
+            //         <input className={style.FormInput}  onChange={modificarEstado} type="text" placeholder="Digite o nome do seu estado" /> 
+            //         <p className={style.FormParagrafo}>O que você sente?</p>
+            //     </div>
+
+            //     <div>
+            //         <input type="checkbox" /> Tremor <br />
+            //         <input type="checkbox" /> Cansaço <br />
+            //         <input type="checkbox" /> Desanimo <br />
+            //         <input type="checkbox" /> Falta de ar <br />
+            //         <input type="checkbox" /> Agonia <br />
+            //         <input type="checkbox" /> Falta de foco <br />
+            //         <input type="checkbox" /> Alteração de humor <br />
+            //         <input type="checkbox" /> Sensação de desconexão <br />
+            //         <input type="checkbox" /> Preocupação com peso <br />
+            //         <input type="checkbox" /> Perda de interesse <br />
+            //         <input type="checkbox" /> Sofri abuso psicologico <br />
+            //         <input type="checkbox" /> Sofri abuso fisico <br />
+            //         <input type="checkbox" /> Sofri abuso sexual <br />
+            //         <input type="checkbox" /> Sofri abuso patrimonial <br />
+            //         <input type="checkbox" /> Sofri abuso moral <br /><br />
+            //     </div>
+
+            //         <button className={style.btoFormAjuda} >Enviar</button>
+            //         <button className={style.btoFormAjuda}>Limpar</button>
+            //         <br />
+                
+            // </div>
