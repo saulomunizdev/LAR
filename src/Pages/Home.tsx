0e1ba242d54f456229743c8e970856399e8f32c7
JEAN RAFAEL LEANDRO BRITES <jeanrleandro@gmail.com>
2025-09-24 22:26:29 -0300

    hoje trabalhei no footer da página inicial

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,10 @@
 import MenuCards from '../Components/MenuCards'
-import Rodape from '../Components/Rodape';
 import '../Style/style.css'
 import HomeInicio from '../Components/hero';
 import TiposAbusos from '../Components/tiposAbusos';
 import PerguntasFrequentes from '../Components/perguntasFrequentes';
 import CabecalhoHome from '../Components/CabecalhoHome';
+import RodapeHome from '../Components/RodapeHome';
 
 
 function Home () {
@@ -20,7 +20,8 @@ function Home () {
 
            <PerguntasFrequentes/>
 
-            {/* <Rodape /> */}
+           <RodapeHome/>
+
         </div>
     )
 }
